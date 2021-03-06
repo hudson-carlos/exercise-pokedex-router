aed7919548a2ad21c2eeda2c32fadd20f86bc592
Hudson Carlos <hc88806@gmail.com>
2021-03-06 14:24:33 -0400

    chage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
 
 function App() {
   return (
-    <div className="App">
-      <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
-    </div>
+    <BrowserRouter>
+      <Route path="/" render={()=><Pokedex pokemons={pokemons} />}>
+      </Route>
+    </BrowserRouter>
   );
 }
 
